4287cc9ad717af32bc2a83b22f4832856ec7425d
linxiaodong <linxiaodong4474@haixue.com>
2019-10-15 23:26:24 +0800

    feat: add travis ci

---
+++ .travis/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+export TZ='Asia/Shanghai'
+
+# 先 clone 再 commit，避免直接 force commit
+# 不然整个 branch 就总是只有一个 commit，不好看
+git clone -b master git@github.com:buxuku/buxuku.github.io.git .deploy_git
+
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
+
+git add .
+git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+
+git push origin master:master --force --quiet
